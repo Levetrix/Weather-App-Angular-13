a98ccab708abe4c5f4ae730cf5a03708bc03b930
Levetrix <winzenriedt@gmail.com>
2022-12-07 13:10:18 -0500

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 // Material - START
 import { MatRadioModule } from '@angular/material/radio';
 // Material - END
 import { AppComponent } from './app.component';
+import { AppHomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '**',
+    component: AppHomeComponent,
+  },
+  {
+    path: 'home',
+    component: AppHomeComponent,
+  },
+  {
+    path: '',
+    component: AppHomeComponent,
+  },
+  {
+    path: 'location/:id',
+    component: AppHomeComponent,
+  },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatRadioModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatRadioModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [AppComponent, AppHomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
