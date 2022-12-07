c1ecd921f0674942121d8532d9fcc15f9f6dc0fb
Levetrix <winzenriedt@gmail.com>
2022-12-07 12:29:17 -0500

    Create clean app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgxPopperjsModule } from 'ngx-popperjs';
 // Material - START
 import { MatRadioModule } from '@angular/material/radio';
 // Material - END
 import { AppComponent } from './app.component';
-import { TourWizardModule } from 'ngx-tour-wizard';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    MatRadioModule,
-    NgxPopperjsModule,
-    TourWizardModule,
-  ],
+  imports: [BrowserModule, FormsModule, MatRadioModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
--- src/app/models.ts
+++
@@ -1,7 +0,0 @@
-
-
-export enum KikProductStates {
-  'unapproved',
-  'approved',
-  'check',
-}
